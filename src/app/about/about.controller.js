0c028183b2ee5d61fc371f34ae3f7b9280dd049e
Li Site <8lisite@tongji.edu.cn>
2016-12-04 00:22:20 +0800

    666

--- src/app/about/about.controller.js
+++ src/app/about/about.controller.js
@@ -6,13 +6,42 @@
     .controller('AboutController', AboutController);
 
   /** @ngInject */
-  function AboutController($timeout, webDevTec, toastr) {
+  function AboutController($scope, $timeout, webDevTec, toastr) {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1480750228371;
     vm.showToastr = showToastr;
+    vm.colorpicker = {
+      options: {
+        orientation: 'horizontal',
+        min: -100,
+        max: 100,
+        range: 'min'
+      }
+    };
+    vm.lisite="李思特123";
+
+    var RGBChange = function() {
+      $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+    };
+
+    var r = $('#R').slider()
+      .on('slide', RGBChange)
+      .data('slider');
+    var g = $('#G').slider()
+      .on('slide', RGBChange)
+      .data('slider');
+    var b = $('#B').slider()
+      .on('slide', RGBChange)
+      .data('slider');
+
+    $("#ex6").slider();
+    $("#ex6").on("slide", function(slideEvt) {
+      $("#ex6SliderVal").text(slideEvt.value);
+      $("#formText").attr("value",slideEvt.value);
+    });
 
     activate();
 
