15eeabde91f5b48c6daae404f9b59585f27ab77b
Site Li <8lisite@tongji.edu.cn>
2017-05-30 15:43:27 +0800

    完成了bug的修改

--- src/app/about/about.controller.js
+++ src/app/about/about.controller.js
@@ -6,21 +6,26 @@
     .controller('AboutController', AboutController);
 
   /** @ngInject */
-  function AboutController($scope) {
+  function AboutController() {
     var vm = this;
     vm.lisite="李思特123";
 
     var RGBChange = function() {
-      $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+      // $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+      angular.element(document).find('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
     };
 
-    var r = $('#R').slider()
+
+    // var r = $('#R').slider()
+    //   .on('slide', RGBChange)
+    //   .data('slider');
+    var r = angular.element(document).find('#R').slider()
       .on('slide', RGBChange)
       .data('slider');
-    var g = $('#G').slider()
+    var g = angular.element(document).find('#G').slider()
       .on('slide', RGBChange)
       .data('slider');
-    var b = $('#B').slider()
+    var b = angular.element(document).find('#B').slider()
       .on('slide', RGBChange)
       .data('slider');
   }
